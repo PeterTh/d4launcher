312ddd19b1536589e5edcea9c1896161af741473
Peter Thoman <peter@metaclassofnil.com>
2015-06-07 16:26:24 +0200

    Initial commit

---
+++ Launcher.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace d4launcher
+{
+    partial class Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Launcher));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.comboBoxRes = new System.Windows.Forms.ComboBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.checkBoxFullscreen = new System.Windows.Forms.CheckBox();
+            this.checkBoxVsync = new System.Windows.Forms.CheckBox();
+            this.buttonLaunch = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.checkBoxShadows = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::d4launcher.Properties.Resources.Splash;
+            this.pictureBox1.InitialImage = global::d4launcher.Properties.Resources.Splash;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 375);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(185, 401);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(60, 13);
+            this.labelRes.TabIndex = 1;
+            this.labelRes.Text = "Resolution:";
+            // 
+            // comboBoxRes
+            // 
+            this.comboBoxRes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRes.FormattingEnabled = true;
+            this.comboBoxRes.Location = new System.Drawing.Point(298, 398);
+            this.comboBoxRes.Name = "comboBoxRes";
+            this.comboBoxRes.Size = new System.Drawing.Size(148, 21);
+            this.comboBoxRes.TabIndex = 2;
+            // 
+            // checkBoxFullscreen
+            // 
+            this.checkBoxFullscreen.AutoSize = true;
+            this.checkBoxFullscreen.Location = new System.Drawing.Point(188, 443);
+            this.checkBoxFullscreen.Name = "checkBoxFullscreen";
+            this.checkBoxFullscreen.Size = new System.Drawing.Size(74, 17);
+            this.checkBoxFullscreen.TabIndex = 5;
+            this.checkBoxFullscreen.Text = "Fullscreen";
+            this.checkBoxFullscreen.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxVsync
+            // 
+            this.checkBoxVsync.AutoSize = true;
+            this.checkBoxVsync.Location = new System.Drawing.Point(290, 443);
+            this.checkBoxVsync.Name = "checkBoxVsync";
+            this.checkBoxVsync.Size = new System.Drawing.Size(58, 17);
+            this.checkBoxVsync.TabIndex = 6;
+            this.checkBoxVsync.Text = "V-sync";
+            this.checkBoxVsync.UseVisualStyleBackColor = true;
+            // 
+            // buttonLaunch
+            // 
+            this.buttonLaunch.Location = new System.Drawing.Point(563, 441);
+            this.buttonLaunch.Name = "buttonLaunch";
+            this.buttonLaunch.Size = new System.Drawing.Size(75, 23);
+            this.buttonLaunch.TabIndex = 7;
+            this.buttonLaunch.Text = "Launch";
+            this.buttonLaunch.UseVisualStyleBackColor = true;
+            this.buttonLaunch.Click += new System.EventHandler(this.buttonLaunch_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(12, 441);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 8;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // checkBoxShadows
+            // 
+            this.checkBoxShadows.AutoSize = true;
+            this.checkBoxShadows.Location = new System.Drawing.Point(376, 443);
+            this.checkBoxShadows.Name = "checkBoxShadows";
+            this.checkBoxShadows.Size = new System.Drawing.Size(70, 17);
+            this.checkBoxShadows.TabIndex = 9;
+            this.checkBoxShadows.Text = "Shadows";
+            this.checkBoxShadows.UseVisualStyleBackColor = true;
+            // 
+            // Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 476);
+            this.Controls.Add(this.checkBoxShadows);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonLaunch);
+            this.Controls.Add(this.checkBoxVsync);
+            this.Controls.Add(this.checkBoxFullscreen);
+            this.Controls.Add(this.comboBoxRes);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Launcher";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "D4 Launcher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.ComboBox comboBoxRes;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.CheckBox checkBoxFullscreen;
+        private System.Windows.Forms.CheckBox checkBoxVsync;
+        private System.Windows.Forms.Button buttonLaunch;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.CheckBox checkBoxShadows;
+    }
+}
+
---
+++ Launcher.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+namespace d4launcher
+{
+    public partial class Launcher : Form
+    {
+        public Launcher()
+        {
+            InitializeComponent();
+            comboBoxRes.Items.AddRange(Resolutions.getResolutions().ToArray());
+            comboBoxRes.SelectedItem = comboBoxRes.Items[0];
+
+
+            var ini = new IniFile("config.ini");
+            var resStr = ini.Read("Resolution", "config");
+            if (resStr.Length > 0)
+            {
+                var res = Resolution.FromIni(resStr);
+                if (comboBoxRes.Items.Contains(res)) comboBoxRes.SelectedItem = res;
+            }
+            try
+            {
+                checkBoxFullscreen.Checked = !Boolean.Parse(ini.Read("window", "config"));
+                checkBoxVsync.Checked = Boolean.Parse(ini.Read("Vsync", "config"));
+                checkBoxShadows.Checked = Boolean.Parse(ini.Read("Shadow", "config"));
+            } catch(Exception) { }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonLaunch_Click(object sender, EventArgs e)
+        {
+            var res = ((Resolution)comboBoxRes.SelectedItem);
+
+            var ini = new IniFile("config.ini");
+            ini.Write("Resolution", res.ToIni(), "config");
+            ini.Write("window", (!checkBoxFullscreen.Checked).ToString(), "config");
+            ini.Write("Vsync", checkBoxVsync.Checked.ToString(), "config");
+            ini.Write("Shadow", checkBoxShadows.Checked.ToString(), "config");
+
+            string args = res.ToArgs();
+            if(checkBoxFullscreen.Checked) args += " -fullscreen";
+            else args += " -windowed";
+            if(!checkBoxVsync.Checked) args += " -novsync";
+            Process.Start(@"Binaries\Win64\D4Game.exe", args);
+            Close();
+        }
+    }
+
+    public class Resolution
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Resolution(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+        public override bool Equals(System.Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Resolution r = obj as Resolution;
+            if ((System.Object)r == null)
+            {
+                return false;
+            }
+
+            return (Width == r.Width) && (Height == r.Height);
+        }
+        public override int GetHashCode()
+        {
+            return Width*10000+Height;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} x {1}", Width, Height);
+        }
+
+        public string ToIni()
+        {
+            return String.Format("{0}-{1}", Width, Height);
+        }
+        public static Resolution FromIni(string val)
+        {
+            var split = val.Split('-');
+            int w = Int32.Parse(split[0]);
+            int h = Int32.Parse(split[1]);
+            return new Resolution(w,h);
+        }
+
+        public string ToArgs()
+        {
+            return String.Format("-resx={0} -resy={1}", Width, Height);
+        }
+    }
+
+    class Resolutions
+    {
+        [DllImport("user32.dll")]
+        static extern bool EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+        const int ENUM_CURRENT_SETTINGS = -1;
+        const int ENUM_REGISTRY_SETTINGS = -2;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct DEVMODE
+        {
+            private const int CCHDEVICENAME = 0x20;
+            private const int CCHFORMNAME = 0x20;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public ScreenOrientation dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+
+        public static List<Resolution> getResolutions()
+        {
+            var ret = new List<Resolution>();
+            var dm = new DEVMODE();
+            int i = 0;
+            while (EnumDisplaySettings(null, i, ref dm))
+            {
+                var res = new Resolution(dm.dmPelsWidth, dm.dmPelsHeight);
+                if (!ret.Contains(res) && res.Height >= 720) ret.Add(res);
+                i++;
+            }
+            return ret;
+        }
+    }
+
+    class IniFile
+    {
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32")]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32")]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFile(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+        }
+
+        public string Read(string Key, string Section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+        }
+
+        public void DeleteSection(string Section = null)
+        {
+            Write(null, null, Section ?? EXE);
+        }
+
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+}
